583d12209679cb3e51c7680bcc212cf838edbbfb
pan_ma_ru <pan_ma_ru@163.com>
2026-09-13 23:36:45 +0800

    feat(parser): 实现 INI 解析器

--- parser/iniparser.cpp
+++ parser/iniparser.cpp
@@ -1,8 +1,97 @@
 #include "iniparser.h"
 
-bool IniParser::parse(const std::string_view &sv) { return false; }
+#include "split.h"
+#include "trim.h"
 
-std::optional<std::string> IniParser::get(const std::string_view &section,
-                                          const std::string_view &key) {
-    return std::nullopt;
+void IniParser::clear() { m_data_.clear(); }
+
+bool IniParser::parse(const std::string_view &sv) {
+    using iniparser::utils::string::split;
+    using iniparser::utils::string::trim;
+
+    clear();
+    std::string currentSection = "";
+    m_data_.emplace(currentSection, ini_section_t{});
+
+    auto lines = split(sv, '\n');
+    for (auto line : lines) {
+        auto trimmedLine = trim(line);
+        bool ok = consumeLine(trimmedLine, currentSection);
+        if (!ok) {
+            clear();
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::optional<std::string> IniParser::get(const std::string &section,
+                                          const std::string &key) {
+    auto secItr = m_data_.find(section);
+    if (secItr == m_data_.end()) {
+        return std::nullopt;
+    }
+
+    auto keyItr = secItr->second.find(key);
+    if (keyItr == secItr->second.end()) {
+        return std::nullopt;
+    }
+
+    return keyItr->second;
+}
+
+bool IniParser::consumeLine(const std::string_view line,
+                            std::string &currentSection) {
+    using iniparser::utils::string::split;
+    using iniparser::utils::string::trim;
+
+    // Ignore empty lines
+    if (line.empty()) {
+        return true;
+    }
+
+    // Section
+    if (line[0] == '[') {
+        return consumeSection(line, currentSection);
+    }
+
+    // Key-value
+    return consumeKeyValue(line, currentSection);
+}
+
+bool IniParser::consumeSection(const std::string_view line,
+                               std::string &currentSection) {
+    using iniparser::utils::string::split;
+    using iniparser::utils::string::trim;
+
+    size_t endPos = line.find(']', 1);
+    if (endPos == std::string::npos) {
+        return false;
+    }
+
+    currentSection = trim(line.substr(1, endPos - 1));
+    m_data_.emplace(currentSection, ini_section_t{});
+    return true;
+}
+
+bool IniParser::consumeKeyValue(const std::string_view line,
+                                std::string &currentSection) {
+    using iniparser::utils::string::split;
+    using iniparser::utils::string::trim;
+
+    auto splits = split(line, '=');
+    if (splits.size() < 3) {
+        return false;
+    }
+
+    auto key = trim(splits[0]);
+    auto value = trim(splits[2]);
+    if (key.empty() || value.empty()) {
+        return false;
+    }
+
+    m_data_.at(currentSection).emplace(key, value);
+
+    return true;
 }
--- parser/iniparser.h
+++ parser/iniparser.h
@@ -8,12 +8,19 @@
 
 class IniParser {
 public:
+    void clear();
     bool parse(const std::string_view &sv);
-    std::optional<std::string> get(const std::string_view &section,
-                                   const std::string_view &key);
+    std::optional<std::string> get(const std::string &section,
+                                   const std::string &key);
 
 private:
     using ini_section_t = std::unordered_map<std::string, std::string>;
+
+    bool consumeLine(const std::string_view line, std::string &currentSection);
+    bool consumeSection(const std::string_view line,
+                        std::string &currentSection);
+    bool consumeKeyValue(const std::string_view line,
+                         std::string &currentSection);
 
     std::unordered_map<std::string, // section name
                        ini_section_t>
